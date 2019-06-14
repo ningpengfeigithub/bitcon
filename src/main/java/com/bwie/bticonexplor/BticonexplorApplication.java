d609291338dd0d0d0a0caf58608ca003a3412487
aa <ssss>
2019-06-14 22:04:07 +0800

    first sql

---
+++ src/main/java/com/bwie/bticonexplor/BticonexplorApplication.java
@@ -0,0 +1,13 @@
+package com.bwie.bticonexplor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BticonexplorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BticonexplorApplication.class, args);
+	}
+
+}
